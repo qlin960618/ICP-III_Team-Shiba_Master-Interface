be7fd4e429e19916e4cf1d9cd489c9ea90648a95
Shakif-a <84608878+Shakif-a@users.noreply.github.com>
2021-07-20 09:27:11 +0600

    Update master_interface_2_control_vrep.py
    Added functions for all values of the potentiometers. Did not fully test the gripper working yet.

--- Master_Interface_2/master_interface_2_control_vrep.py
+++ Master_Interface_2/master_interface_2_control_vrep.py
@@ -1,38 +1,47 @@
-from dqrobotics.interfaces.vrep import DQ_VrepInterface
-from dqrobotics.robot_modeling import DQ_Kinematics
-from umirobot_task_space_control.umirobot_vrep_robot import UMIRobotVrepRobot
-from dqrobotics import *
-from dqrobotics.solvers import DQ_QuadprogSolver
-
+"""
+Copyright (C) 2020 Murilo Marques Marinho (www.murilomarinho.info)
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+version.
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+You should have received a copy of the GNU General Public License along with this program. If not,
+see <https://www.gnu.org/licenses/>.
+"""
 import numpy as np
-from umirobot import UMIRobot
+from dqrobotics import *
 from math import *
 
-# Change these values to scale the mapping how you like
-# master 2 lengths
-l1 = 150 / 1000
-l2 = 150 / 1000
-# linear conversion to rad variables
-m = 1.012290966123
-c = -2.530727415
-# linear conversion to z-axis translation
-c2 = -165 / 1000
-m2 = 66/1000
+from umirobot.shared_memory import UMIRobotSharedMemoryReceiver
+from umirobot_task_space_controller import UMIRobotTaskSpaceController
+from dqrobotics.interfaces.vrep import DQ_VrepInterface
+from dqrobotics.utils.DQ_Math import rad2deg
+from umirobot_vrep_robot import UMIRobotVrepRobot
 
 
 def master_control_two(pot1, pot2, pot3):
     """ This function takes the three potentiometer values from the master interface 2 of the
     unparalleled shibas and calculates the pose transformation of the end effector of the master robot.
     Using that it calculates and returns the unit DQ pose transformation of the target."""
-
+    # Change these values to scale the mapping how you like
+    # master 2 lengths
+    l1 = 150 / 1000
+    l2 = 150 / 1000
+    # linear conversion to rad variables
+    m = 1.012290966123
+    c = -2.530727415
+    # linear conversion to z-axis translation
+    c2 = -165 / 1000
+    m2 = 66 / 1000
     # Convert pot values to approximate angles in radians
+
     theta1 = m * pot1 + c
     theta2 = -m * pot2 - c
     # Convert pot3 for vertical translation
     theta3 = m2 * pot3 + c2
     # Find the rotation transformation DQs
     r1 = cos(theta1 / 2) + sin(theta1 / 2) * i_
-    r2 = cos(theta2 / 2) + sin(theta2 / 2) * i_
+    r2 = cos(theta2 / 2) + sin(theta2 / 2) * -i_
     # Find the translation DQs
     t1 = 1 + 0.5 * E_ * (l1 * -j_)
     t2 = 1 + 0.5 * E_ * (l2 * -j_)
@@ -44,145 +53,122 @@ def master_control_two(pot1, pot2, pot3):
     return x_t
 
 
-def _get_rotation_error(x, xd):
-    # Calculate error from invariant
-    error_1 = vec4(rotation(x) - rotation(xd))
-    error_2 = vec4(rotation(x) + rotation(xd))
+def gripper_calc(pot4, pot5, pot6):
+    "A function to convert the 0-5 V potentiometer values to integer angles in degrees."
+    m3 = 36
+    c3 = -90
+    gripper_qd = [0, 0, 0]
+    gripper_qd[0] = int(36 * pot4 - 90)
+    gripper_qd[1] = int(36 * pot5 - 90)
+    gripper_qd[2] = int(36 * pot6 - 90)
 
-    # Calculate 'distance' from invariant
-    norm_1 = np.linalg.norm(error_1)
-    norm_2 = np.linalg.norm(error_2)
-
-    # Check the closest invariant and return the proper error
-    if norm_1 < norm_2:
-        return error_1
-    else:
-        return error_2
+    return gripper_qd
 
 
-# Instantiate a DQ_VrepInterface
-vrep_interface = DQ_VrepInterface()
+configuration = {
+    "controller_gain": 4.0,
+    "damping": 0.01,
+    "alpha": 0.999,  # Soft priority between translation and rotation [0,1] ~1 Translation, ~0 Rotation
+    "use_real_umirobot": False,
+    "umirobot_port": "COM3"
+}
 
-try:
-    # Try to connect to VREP
-    if not vrep_interface.connect(20001, 100, 10):
-        # If connection fails, disconnect and throw exception
-        vrep_interface.disconnect_all()
-        raise Exception("Unable to connect to VREP.")
 
-    # This object is used to communicate more easily with VREP
-    umirobot_vrep = UMIRobotVrepRobot(vrep_interface=vrep_interface)
+def control_loop(umirobot_smr, cfg):
+    # Instantiate a DQ_VrepInterface
+    vrep_interface = DQ_VrepInterface()
 
-    # This is a DQ_SerialManipulatorDH instance. Used to calculate kinematics of the UMIRobot.
-    umirobot_kinematics = umirobot_vrep.kinematics()
+    try:
+        # Try to connect to robot
+        if cfg["use_real_umirobot"]:
+            umirobot_smr.send_port(cfg["umirobot_port"])
+            print("main::Trying to connect to umirobot at port={}.".format(cfg["umirobot_port"]))
 
-    # QP Solver
-    qp_solver = DQ_QuadprogSolver()
+        # Try to connect to VREP
+        if not vrep_interface.connect(20000, 100, 10):
+            # If connection fails, disconnect and throw exception
+            vrep_interface.disconnect_all()
+            raise Exception("Unable to connect to VREP.")
 
-    # Initialize the objects in VREP to reflect what we calculate using DQRobotics
-    q_init = umirobot_vrep.get_q_from_vrep()
-    x_init = umirobot_kinematics.fkm(q_init)
-    umirobot_vrep.show_x_in_vrep(x_init)
-    umirobot_vrep.show_xd_in_vrep(x_init)
+        # This object is used to communicate more easily with VREP
+        umirobot_vrep = UMIRobotVrepRobot(vrep_interface=vrep_interface)
 
-    # Controller parameters (Change these to see behavior if desired)
-    eta = 4
-    damping = 0.01
-    sampling_time = 0.008
-    alpha = 0.999  # close to 1 means translation is prioritised
+        # This is a DQ_SerialManipulatorDH instance. Used to calculate kinematics of the UMIRobot.
+        umirobot_kinematics = umirobot_vrep.kinematics()
 
-    # Control loop (We're going to control it open loop, because that is how we operate the real robot)
-    # Initialize q with its initial value
-    q = q_init
-    motion_scaling = 1.75
+        # UMIRobot Task Space Controler
+        umirobot_controller = UMIRobotTaskSpaceController(kinematics=umirobot_kinematics)
+        umirobot_controller.set_gain(cfg["controller_gain"])
+        umirobot_controller.set_damping(cfg["damping"])
+        umirobot_controller.set_alpha(cfg["alpha"])
 
-    if __name__ == '__main__':
-        with UMIRobot() as umirobot:
-            umirobot.set_port('COM3')
-            while True:
-                try:
-                    # Master ref to apply pose transform
-                    x_master_ref = vrep_interface.get_object_pose("x_master_ref")
+        # Initialize the objects in VREP to reflect what we calculate using DQRobotics
+        q_init = umirobot_vrep.get_q_from_vrep()
+        x_init = umirobot_kinematics.fkm(q_init)
+        umirobot_vrep.show_x_in_vrep(x_init)
+        umirobot_vrep.show_xd_in_vrep(x_init)
 
-                    q1 = umirobot.get_potentiometer_values()
-                    if isinstance(q1[0], float):
-                        x_master = master_control_two(q1[0], q1[1], q1[2])
-                    umirobot.set_qd([10, 10, 10, 10, 10, 10])
-                    umirobot.update()
+        # Loop parameters
+        sampling_time = 0.008
 
-                    # Get current pose information
-                    x = umirobot_kinematics.fkm(q)
-                    # xd = umirobot_vrep.get_xd_from_vrep()
-                    # Motion scaling transformation
-                    """ r_master = rotation(x_master)
-                    t_master = translation(x_master)
-                    tx_master = motion_scaling * t_master.q[1]
-                    ty_master = motion_scaling * t_master.q[2]
-                    tz_master = t_master.q[3]
-                    t_master_ms = tx_master * i_ * ty_master * j_ * tz_master * k_
+        # Control loop (We're going to control it open loop, because that is how we operate the real robot)
+        # Initialize q with its initial value
+        q = q_init
+        while True:
+            # Change how you calculate xd
+            q1 = umirobot_smr.get_potentiometer_values()
+            x_master_ref = vrep_interface.get_object_pose("x_master_ref")
+            if isinstance(q1[0], float):
+                x_master = master_control_two(q1[0], q1[1], q1[2])
+                gripper_qd = gripper_calc(q1[3], q1[4], q1[5])
 
-                    x_master_ms = r_master + 0.5 * E_ * t_master_ms * r_master"""
+            # xd = umirobot_vrep.get_xd_from_vrep()
+            xd = x_master_ref * x_master
 
-                    xd = x_master_ref * x_master
+            # Solve the quadratic program
+            u = umirobot_controller.compute_setpoint_control_signal(q, xd)
 
-                    # Calculate errors
-                    et = vec4(translation(x) - translation(xd))
-                    er = _get_rotation_error(x, xd)
+            # Update the current joint positions
+            q = q + u * sampling_time
+            # Manually change last 3 joints
+            q[3] = gripper_qd[0]
+            q[4] = gripper_qd[1]
+            q[5] = gripper_qd[2]
 
-                    # Get the Translation Jacobian and Rotation Jacobian
-                    Jx = umirobot_kinematics.pose_jacobian(q)
-                    Jr = DQ_Kinematics.rotation_jacobian(Jx)
-                    Jt = DQ_Kinematics.translation_jacobian(Jx, x)
+            # Update vrep with the new information we have
+            umirobot_vrep.send_q_to_vrep(q)
+            umirobot_vrep.show_x_in_vrep(umirobot_controller.get_last_robot_pose())
+            umirobot_vrep.show_xd_in_vrep(xd)
 
-                    # ++++++++++++++++Calculate the control step according to++++++++++++++++
-                    # "A Unified Framework for the Teleoperation of Surgical Robots in Constrained Workspaces".
-                    # Marinho, M. M; et al.
-                    # In 2019 IEEE International Conference on Robotics and Automation (ICRA), pages 2721–2727, May 2019. IEEE
-                    # http://doi.org/10.1109/ICRA.2019.8794363
+            # Update real robot if needed
+            if cfg["use_real_umirobot"]:
+                if umirobot_smr.is_open():
+                    # Get the desired gripper value from VREP
+                    gripper_value_d = umirobot_vrep.get_gripper_value_from_vrep()
+                    # Control the gripper somehow
+                    q_temp = np.hstack((q, gripper_value_d))
+                    # The joint information has to be sent to the robot as a list of integers
+                    umirobot_smr.send_qd(rad2deg(q_temp).astype(int).tolist())
+                else:
+                    raise Exception("UMIRobot port not opened at {}.".format(cfg["umirobot_port"]))
 
-                    # Translation term
-                    Ht = Jt.transpose() @ Jt + np.eye(6, 6) * damping
-                    ft = eta * Jt.transpose() @ et
+    except Exception as e:
+        print("Exception caught: ", e)
+    except KeyboardInterrupt:
+        print("KeyboardInterrupt")
 
-                    # Rotation term
-                    Hr = Jr.transpose() @ Jr + np.eye(6, 6) * damping
-                    fr = eta * Jr.transpose() @ er
+    # Disconnect from VREP
+    vrep_interface.disconnect()
 
-                    # Combine terms using the soft priority
-                    H = alpha * Ht + (1.0 - alpha) * Hr
-                    f = alpha * ft + (1.0 - alpha) * fr
 
-                    # Joint (position) limit constraints
-                    lower_joint_limits = -np.pi / 2.0 * (np.ones((6,)))
-                    upper_joint_limits = np.pi / 2.0 * (np.ones((6,)))
-                    W_jl = np.vstack((-1.0 * np.eye(6, 6), np.eye(6, 6)))
-                    w_jl = np.hstack((-1.0 * (lower_joint_limits - q), 1.0 * (upper_joint_limits - q)))
+def run(shared_memory_info, lock):
+    umirobot_smr = UMIRobotSharedMemoryReceiver(shared_memory_info, lock)
 
-                    # Solve the quadratic program
-                    u = qp_solver.solve_quadratic_program(H, f, W_jl, w_jl, np.zeros((1, 6)), np.zeros(1))
+    try:
+        control_loop(umirobot_smr, cfg=configuration)
+    except Exception as e:
+        print("umirobot_task_space_control::run::Error::" + str(e))
+    except KeyboardInterrupt:
+        print("umirobot_task_space_control::run::Info::Interrupted by user.")
 
-                    # ++++++++++++++++End of the control calculation++++++++++++++++
-
-                    # Update the current joint positions
-                    q = q + u * sampling_time
-
-                    # Update vrep with the new information we have
-                    umirobot_vrep.send_q_to_vrep(q)
-                    umirobot_vrep.show_x_in_vrep(x)
-                    umirobot_vrep.show_xd_in_vrep(xd)
-                except Exception as e:
-                    print("umirobot_test::Error::" + str(e))
-                except KeyboardInterrupt:
-                    print("umirobot_test::Info::Execution ended by user.")
-                    print("Move the robot back to 0 degrees in each joint.")
-                    umirobot.set_qd([0, 0, 0, 0, 0, 0])
-                    umirobot.update()
-                    break
-
-except Exception as e:
-    print("Exception caught: ", e)
-except KeyboardInterrupt:
-    print("KeyboardInterrupt")
-
-# Disconnect from VREP
-vrep_interface.disconnect()
+    umirobot_smr.send_shutdown_flag(True)
